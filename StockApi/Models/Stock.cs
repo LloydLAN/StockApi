74923220570200148e6e9313e5bfb45e292de10e
LloydLAN <allan.lan456@gmail.com>
2021-04-12 23:06:41 +0800

    First Version

---
+++ StockApi/Models/Stock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockApi.Models
+{
+    public class Stock
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<DividendPerYear> DividendData { get; set; }
+
+        public class DividendPerYear
+        {
+            [Key]
+            public string Year { get; set; }
+
+            public string CashDividend { get; set; }
+
+            public string StockDividend { get; set; }
+
+            public string TotalDividend { get; set; }
+        }
+    }
+}
---
+++ StockApi/Models/StockContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StockApi.Models
+{
+    public class StockContext : DbContext
+    {
+        public StockContext(DbContextOptions<StockContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+    }
+}
